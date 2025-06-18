2dda9ab6052c69f4c084599316fa84cce4a763a3
김강민 <122205332+kkangmen@users.noreply.github.com>
2025-06-18 18:43:22 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void solution(){
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a+b);
+    }
+    public static void main(String[] args){
+        solution();
+    }
+}
