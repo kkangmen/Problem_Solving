ad144cb7313f0bbd1b41558a9f116631b0e27c1f
김강민 <122205332+kkangmen@users.noreply.github.com>
2025-08-06 14:46:51 +0900

    [level 1] Title: 폰켓몬, Time: 5.66 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i : nums){
+            set.add(i);
+        }
+        
+        answer = Math.min(set.size(), nums.length/2);
+        return answer;
+    }
+}
