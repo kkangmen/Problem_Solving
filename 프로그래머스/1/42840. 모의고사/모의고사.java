ce6e612d2ef0c8ea60a05ad2a2eac424800a07b7
김강민 <122205332+kkangmen@users.noreply.github.com>
2025-09-08 16:44:57 +0900

    [level 1] Title: 모의고사, Time: 4.31 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,50 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] answers) {
-        List<Integer> answer = new LinkedList<>();
+    public int[] solution(int[] answer) {
         
-        int[] num1 = {1, 2, 3, 4, 5};
-        int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] total = new int[3];
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
         
-        int i = 0, j = 0, k = 0;
-        for (int index : answers){
-            if (index == num1[i]){
-                total[0]++;
-            }
-            if (index == num2[j]){
-                total[1]++;
-            }
-            if (index == num3[k]){
-                total[2]++;
-            }
-            
-            i++;
-            j++;
-            k++;
-            if (i == 5){
-                i = 0;
-            }
-            if (j == 8){
-                j = 0;
-            }
-            if (k == 10){
-                k = 0;
-            }
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == a[i%a.length]) {score[0]++;}
+            if(answer[i] == b[i%b.length]) {score[1]++;}
+            if(answer[i] == c[i%c.length]) {score[2]++;}
         }
         
-        int max = 0;
-        for (int num : total){
-            max = Math.max(max, num);
-        }
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
         
-        for (int num = 0; num < 3; num++){
-            if (total[num] == max){
-                answer.add(num+1);
-            }
-        }
-        return answer;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        if(maxScore == score[0]) {list.add(1);}
+        if(maxScore == score[1]) {list.add(2);}
+        if(maxScore == score[2]) {list.add(3);}
+        
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
 }
