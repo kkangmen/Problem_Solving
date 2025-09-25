1463d9b7c604936d630e9e4d44ecf9b50a55cf3f
김강민 <122205332+kkangmen@users.noreply.github.com>
2025-09-25 23:31:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1072 ms, Memory: 172796 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,29 +1,27 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 1. HashMap 준비
-        Map<Integer, Integer> map = new HashMap<>();
-
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
         int N = Integer.parseInt(br.readLine());
-        // 2. StringTokenizer로 입력받아 Map에 바로 저장 (정렬 필요 없음)
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++){
             int cardNum = Integer.parseInt(st.nextToken());
             map.put(cardNum, map.getOrDefault(cardNum, 0) + 1);
         }
-
+        
         int M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++){
             int checkNum = Integer.parseInt(st.nextToken());
-            // 3. Map에서 O(1)으로 바로 개수 조회
             sb.append(map.getOrDefault(checkNum, 0)).append(" ");
         }
-
+        
         System.out.println(sb.toString());
     }
 }
