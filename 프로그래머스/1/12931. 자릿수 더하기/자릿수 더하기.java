ca611232352927023d9b16414cb9198b8a81de87
김강민 <122205332+kkangmen@users.noreply.github.com>
2025-10-22 16:51:36 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,10 +4,10 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        String s = Integer.toString(n);
-        
-        for (int i = 0; i < s.length(); i++){
-            answer += Character.getNumericValue(s.charAt(i));
+        while(n > 0){
+            answer += n%10;
+            
+            n = n/10;
         }
         
         return answer;
