bae04690f45d524d583b1c4d34bb3c6c45d67413
김강민 <122205332+kkangmen@users.noreply.github.com>
2025-10-24 12:55:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int num = x;
+        int sum = 0;
+        while (num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        
+        if (x%sum != 0){
+            answer = false;
+        }
+        return answer;
+    }
+}
