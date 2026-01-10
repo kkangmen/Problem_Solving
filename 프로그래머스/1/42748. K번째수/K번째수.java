8ed2239cf3b85494ef2352bb08696cf517eafdf1
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-01-10 21:58:34 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,25 +3,16 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int[] array, int[][] commands) {
         List<Integer> answer = new ArrayList<>();
+        int[] subArray = {};
         
-        for (int i = 0; i < commands.length; i++){
-            int[] row = commands[i];
-            
+        for (int[] row : commands){
             int start = row[0];
             int end = row[1];
-            int index = row[2];
+            int pick = row[2];
             
-            int[] subArray = {};
             subArray = Arrays.copyOfRange(array, start-1, end);
-            
             Arrays.sort(subArray);
-            answer.add(subArray[index-1]);
-            
-            // 출력
-            // for (int j : subArray){
-            //     System.out.print(j + " ");
-            // }
-            // System.out.println();
+            answer.add(subArray[pick-1]);
         }
         return answer;
     }
