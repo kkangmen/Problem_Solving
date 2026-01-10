d26f8d8eefd30d0f78770011d1af0ef878f2f732
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-01-10 18:00:06 +0900

    [level 1] Title: 실패율, Time: 8.80 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,19 +1,23 @@
 import java.util.*;
 
 class Solution {
-    public class difficulty {
-        int stage;
-        double percentage;
-        
-        difficulty(int stage, double percentage){
-            this.stage = stage;
-            this.percentage = percentage;
+    public class difficulty{
+        private int stages;
+        private double failure;
+
+        difficulty(int stages, double failure){
+            this.stages = stages;
+            this.failure = failure;
         }
     }
     
     public List<Integer> solution(int N, int[] stages) {
         List<Integer> answer = new ArrayList<>();
-
+        List<difficulty> list = new ArrayList<>();
+        //         1  2  3  4  5  6
+        // trying [1  3  2  1  0  1]
+        // dp     [8  7  4  2  1  1]
+        
         int[] trying = new int[N+2];
         int[] dp = new int[N+2];
         
@@ -26,32 +30,32 @@ class Solution {
             dp[i] = dp[i+1] + trying[i];
         }
         
-        List<difficulty> list = new ArrayList<>();
         for (int i = 1; i <= N; i++){
             if (dp[i] == 0){
                 list.add(new difficulty(i, 0));
+                continue;
             }
-            else {
-                double d = (double)trying[i] / (double)dp[i];
-                list.add(new difficulty(i, d));
-            }
+            
+            double d = (double)trying[i]/dp[i];
+            
+            list.add(new difficulty(i, d));
         }
         
         Collections.sort(list, (difficulty a, difficulty b) -> {
-            if (a.percentage == b.percentage){
-                return Integer.compare(a.stage, b.stage);
-            }
-            return Double.compare(b.percentage, a.percentage);
+           if (a.failure == b.failure){
+                return Integer.compare(a.stages, b.stages);   
+           }
+            return Double.compare(b.failure, a.failure);
         });
         
-        // 출력
         Iterator<difficulty> iter = list.iterator();
-        
-        while(iter.hasNext()){
+        while (iter.hasNext()){
             difficulty d = iter.next();
-            answer.add(d.stage);
-            System.out.println(d.stage + ", " + d.percentage);
+            answer.add(d.stages);
         }
+        // for (int i = 1; i <= N; i++){
+        //     System.out.print(dp[i] + " ");
+        // }
         return answer;
     }
 }
