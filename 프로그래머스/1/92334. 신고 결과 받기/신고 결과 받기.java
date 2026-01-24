21baa2c6c53b02a578cda60c0f1a75c0a4f9bf89
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-01-24 15:41:54 +0900

    [level 1] Title: 신고 결과 받기, Time: 770.91 ms, Memory: 179 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,31 +4,40 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
-        Set<String> reportSet = new HashSet<>(Arrays.asList(report));
+        Set<String> reportSet = new HashSet<>(List.of(report));
+        Map<String, Integer> reportMap = new HashMap<>();
         
-        Map<String, Integer> reportCount = new HashMap<>();
+        for (String s : id_list){
+            reportMap.put(s, 0);
+        }
+        
         for (String s : reportSet){
             String reported = s.split(" ")[1];
             
-            reportCount.put(reported, reportCount.getOrDefault(reported, 0) + 1);
+            reportMap.put(reported, reportMap.getOrDefault(reported, 0) + 1);
         }
         
-        Set<String> bannedList = new HashSet<>();
-        for (String s : reportCount.keySet()){
-            if (reportCount.get(s) >= k){
+        List<String> bannedList = new ArrayList<>();
+        for (String s : id_list){
+            if (k <= reportMap.get(s)){
                 bannedList.add(s);
             }
         }
         
+        List<String> id_listArray = List.of(id_list);
         for (String s : reportSet){
             String reporter = s.split(" ")[0];
             String reported = s.split(" ")[1];
             
+            int index = id_listArray.indexOf(reporter);
             if (bannedList.contains(reported)){
-                int index = Arrays.asList(id_list).indexOf(reporter);
                 answer[index]++;
             }
         }
+        // Iterator<String> iter = reportSet.iterator();
+        // while (iter.hasNext()){
+        //     System.out.println(iter.next());
+        // }
         return answer;
     }
 }
