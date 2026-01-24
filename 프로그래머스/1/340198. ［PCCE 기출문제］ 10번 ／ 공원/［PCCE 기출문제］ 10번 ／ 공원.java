a6b503b555a4f2d52a4cd18f1f47c27f50673428
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-01-24 15:01:37 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 4.81 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,39 +1,44 @@
 import java.util.*;
 
 class Solution {
-    public boolean findPark(int row, int col, String[][] park, int size){
-        if (row + size > park.length || col + size > park[0].length){
+    public boolean canFit(int row, int col, String[][] park, int size){
+        if (row+size > park.length || col+size > park[0].length){
             return false;
         }
-        
+            
         for (int i = row; i < row+size; i++){
             for (int j = col; j < col+size; j++){
                 if (!park[i][j].equals("-1")){
                     return false;
                 }
             }
-        }
+        }  
         return true;
     }
     
     public int solution(int[] mats, String[][] park) {
-        int answer = 0;
-        
+        int answer = -1;
         Arrays.sort(mats);
-        for (int i = mats.length-1; i >= 0; i--){
-            int mat = mats[i];
-            
-            for (int j = 0; j < park.length; j++){
-                for (int k = 0; k < park[0].length; k++){
-                    if (park[j][k].equals("-1")){
-                        if (findPark(j, k, park, mat)){
-                            answer = mat;
-                            return answer;
+        
+        int parkRow = park.length;
+        int parkCol = park[0].length;
+        
+        for (int i = 0; i < parkRow; i++){
+            for (int j = 0; j < parkCol; j++){
+                if(park[i][j].equals("-1")){
+                    for (int k = 0; k < mats.length; k++){
+                        if(canFit(i, j, park, mats[k])){
+                            answer = Math.max(answer, mats[k]);
                         }
                     }
-                }
+                }        
             }
         }
-        return -1;
+        
+        if (answer == -1){
+            return -1;
+        }
+        return answer;
     }
 }
+// 50*50*10*20*20 = 25000*400 = 10,000,000
