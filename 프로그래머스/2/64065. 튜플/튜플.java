3de0a6f04d40d36c7de4e4d7dbe495ac6f2a8b9f
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-02-16 16:23:44 +0900

    [level 2] Title: 튜플, Time: 86.55 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,45 +2,23 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(String s) {
-        List<String[]> list = new ArrayList<>();
-        
-        s = s.substring(1, s.length()-1);
-        
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            
-            if (ch == '{'){
-                start = i;
-            }
-            if (ch == '}'){
-                end = i;
-                String[] temp = s.substring(start+1, end).split(",");
-                list.add(temp);
-            }
-        }
         List<Integer> answer = new ArrayList<>();
+        Set<String> set = new HashSet<>();
         
-        Collections.sort(list, (a, b) -> {
-            return a.length - b.length;
+        String[] elements = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        
+        Arrays.sort(elements, (a, b) -> {
+            return a.length() - b.length();
         });
-        
-        String[] tempArr = list.get(list.size()-1);
-        Map<String, Integer> map = new HashMap<>();
-        for (String k : tempArr){
-            map.put(k, map.getOrDefault(k, 0) + 1);
-        }
-        
-        for (String[] arr : list){
-            for (String ele : arr){
-                if (map.get(ele) == 1){
-                    answer.add(Integer.parseInt(ele));
-                    map.put(ele, map.getOrDefault(ele, 0) - 1);
+            
+        for (String element : elements){
+            for (String elementWord : element.split(",")){
+                if (set.add(elementWord)){
+                    answer.add(Integer.parseInt(elementWord));
                 }
             }
         }
-
+        
         return answer;
     }
 }
