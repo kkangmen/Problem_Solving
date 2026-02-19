ecc85f206d8cc140f8abfb79252ffa6e85bbd6cd
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-02-19 11:59:09 +0900

    [level 3] Title: 가장 먼 노드, Time: 7525.29 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -5,51 +5,50 @@ class Solution {
     static boolean[] isVisited;
     static int[] distance;
     static Queue<Integer> q = new LinkedList<>();
+    static int maxDistance = 0;
     
     public void bfs(int start){
         isVisited[start] = true;
-        distance[start] = 0;
         q.offer(start);
         
-        while(!q.isEmpty()){
-            int x = q.poll();
-            for (int i = 0; i < graph.get(x).size(); i++){
-                int y = graph.get(x).get(i);
-                if (!isVisited[y]){
-                    q.offer(y);
-                    isVisited[y] = true;
-                    distance[y] = distance[x] + 1;
+        while (!q.isEmpty()){
+            int node = q.poll();
+            
+            for (int i = 0; i < graph.get(node).size(); i++){
+                int n_node = graph.get(node).get(i);
+                if (!isVisited[n_node]){
+                    q.offer(n_node);
+                    isVisited[n_node] = true;
+                    distance[n_node] = distance[node] + 1;
+                    maxDistance = distance[n_node];
                 }
             }
         }
     }
     
     public int solution(int n, int[][] edge) {
-        int count = 0;
+        int answer = 0;
         
-        isVisited = new boolean[n];
-        distance = new int[n];
-        for (int i = 0; i < n; i++){
-            graph.add(new ArrayList<>());
+        isVisited = new boolean[n+1];
+        distance = new int[n+1];
+        
+        for (int i = 0; i <= n; i++){
+            graph.add(new LinkedList<>());
         }
         
         for (int[] e : edge){
-            graph.get(e[0]-1).add(e[1]-1);
-            graph.get(e[1]-1).add(e[0]-1);
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
         }
         
-        bfs(0);
+        bfs(1);
         
-        int max = 0;
-        for (int i = 0; i < n; i++){
-            max = Math.max(max, distance[i]);
-        }
-        
-        for (int i = 0; i < n; i++){
-            if (distance[i] == max){
-                count++;
+        for (int i = 1; i <= n; i++){
+            if (distance[i] == maxDistance){
+                answer++;
             }
         }
-        return count;
+        
+        return answer;
     }
 }
