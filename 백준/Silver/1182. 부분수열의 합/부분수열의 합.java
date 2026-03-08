4c921fa9d230b680159f1ae81547b20ca84712b8
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-03-09 00:54:26 +0900

    [Silver II] Title: 부분수열의 합, Time: 112 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,14 +1,12 @@
 import java.io.*;
 import java.util.*;
-
+    
 public class Main {
-    static int answer = 0;
-    static int N;
-    static int S;
+    static int N, S;
     static int[] arr;
-    static boolean[] isUsed;
+    static int answer = 0;
 
-    public static void dfs(int index, int sum){
+    public static void pruning(int index, int sum){
         if (index == N){
             if (sum == S){
                 answer++;
@@ -16,8 +14,8 @@ public class Main {
             return;
         }
 
-        dfs(index + 1, sum + arr[index]);
-        dfs(index + 1, sum);
+        pruning(index + 1, sum + arr[index]);
+        pruning(index + 1, sum);
     }
 
     public static void solution() throws IOException {
@@ -26,22 +24,21 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
-        arr = new int[N]; // [-7, -3, -2, 5, 8]
-        isUsed = new boolean[N];
+        arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(0, 0);
+        pruning(0, 0);
+
         if (S == 0){
             answer--;
         }
         System.out.println(answer);
         br.close();
     }
-
     public static void main(String[] args) throws IOException {
         solution();
     }
