4c0875c2f50f79c62ecbd15856b45b7140c59e2c
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-03-15 00:14:22 +0900

    [Gold IV] Title: N-Queen, Time: 3992 ms, Memory: 14964 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,22 +1,22 @@
 import java.io.*;
 import java.util.*;
-    
+
 public class Main {
     static int N;
     static int[] row;
     static boolean[] isUsed;
     static int answer = 0;
 
-    public static boolean isDiagnal(int count){
-        for (int i = 0; i < count; i++){
-            if (Math.abs(i - count) == Math.abs(row[i] - row[count])){
-                return true;
+    public static boolean isPossible(int index){
+        for (int i = 0; i < index; i++){
+            if (Math.abs(i - index) == Math.abs(row[i] - row[index])){
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
-    public static void backTracking(int count){
+    public static void permutation(int count){
         if (count == N){
             answer++;
             return;
@@ -24,9 +24,9 @@ public class Main {
 
         for (int i = 0; i < N; i++){
             row[count] = i;
-            if(!isUsed[i] && !isDiagnal(count)){
+            if (!isUsed[i] && isPossible(count)){
                 isUsed[i] = true;
-                backTracking(count+1);
+                permutation(count + 1);
                 isUsed[i] = false;
             }
         }
@@ -34,15 +34,17 @@ public class Main {
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken());
         row = new int[N];
         isUsed = new boolean[N];
 
-        backTracking(0);
+        permutation(0);
         System.out.println(answer);
         br.close();
     }
+
     public static void main(String[] args) throws IOException {
         solution();
     }
