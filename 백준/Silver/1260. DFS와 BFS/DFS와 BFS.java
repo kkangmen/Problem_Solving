3a7e7afe19ea251356f58552b88985f38c0273a9
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-03-20 17:29:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 240 ms, Memory: 21840 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,48 +1,50 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
+    
 public class Main {
+    static int N, M, V;
     static Map<Integer, List<Integer>> graph = new HashMap<>();
     static boolean[] isVisited;
     static Queue<Integer> q = new LinkedList<>();
+    static StringBuilder sb = new StringBuilder();
 
     public static void dfs(int start){
         isVisited[start] = true;
-        System.out.print(start + " ");
+        sb.append(start).append(" ");
 
         for (int i = 0; i < graph.get(start).size(); i++){
-            int x = graph.get(start).get(i);
-            if (!isVisited[x]){
-                dfs(x);
+            int node = graph.get(start).get(i);
+            if (!isVisited[node]){
+                dfs(node);
             }
         }
     }
 
     public static void bfs(int start){
-        q.offer(start);
         isVisited[start] = true;
-        System.out.print(start + " ");
+        q.offer(start);
 
         while (!q.isEmpty()){
-            int x = q.poll();
-            for (int i = 0; i < graph.get(x).size(); i++){
-                int y = graph.get(x).get(i);
-                if(!isVisited[y]){
-                    q.offer(y);
-                    System.out.print(y + " ");
-                    isVisited[y] = true;
+            int node = q.poll();
+            sb.append(node).append(" ");
+
+            for (int i = 0; i < graph.get(node).size(); i++){
+                int newNode = graph.get(node).get(i);
+                if (!isVisited[newNode]){
+                    isVisited[newNode] = true;
+                    q.offer(newNode);
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
         isVisited = new boolean[N+1];
         for (int i = 0; i <= N; i++){
@@ -51,7 +53,6 @@ public class Main {
 
         for (int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
@@ -59,13 +60,18 @@ public class Main {
             graph.get(b).add(a);
         }
 
-        for (int i = 1; i <= N; i++){
-            Collections.sort(graph.get(i));
+        for (List<Integer> list : graph.values()){
+            Collections.sort(list);
         }
 
         dfs(V);
-        isVisited = new boolean[N+1];
-        System.out.println();
+        sb.append("\n");
+        Arrays.fill(isVisited, false);
         bfs(V);
+        System.out.println(sb.toString());
+        br.close();
+    }
+    public static void main(String[] args) throws IOException {
+        solution();
     }
 }
