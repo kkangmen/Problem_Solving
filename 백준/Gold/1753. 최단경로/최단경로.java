5de8323a2fc154b7c0ba374186b781ddf912e34f
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-03-25 21:32:17 +0900

    [Gold IV] Title: 최단경로, Time: 988 ms, Memory: 155256 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,68 +2,72 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static class Node{
+    static int V, E, K;
+    static int[] distance;
+    static HashMap<Integer, List<Node>> graph = new HashMap<>();
+    static PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) ->
+            Integer.compare(o1.cost, o2.cost));
+    private static class Node {
         int idx;
         int cost;
-        Node (int idx, int cost){
+        Node(int idx, int cost){
             this.idx = idx;
             this.cost = cost;
         }
     }
+    public static void djikstra(int start){
+        pq.offer(new Node(start, 0));
+        distance[start] = 0;
 
-    static HashMap<Integer, List<Node>> graph = new HashMap<>();
-    static PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
-    static int[] distance;
+        while (!pq.isEmpty()){
+            Node curNode = pq.poll(); // 1
+
+            for (int i = 0; i < graph.get(curNode.idx).size(); i++){
+                Node nxtNode = graph.get(curNode.idx).get(i); // 2, 3
+
+                if (distance[nxtNode.idx] > distance[curNode.idx] + nxtNode.cost){
+                    distance[nxtNode.idx] = distance[curNode.idx] + nxtNode.cost;
+                    pq.offer(new Node(nxtNode.idx, distance[nxtNode.idx]));
+                }
+            }
+        }
+    }
 
     public static void solution() throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = sc.nextInt();
-        int E = sc.nextInt();
-        int start = sc.nextInt();
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
+        distance = new int[V+1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
 
         for (int i = 0; i <= V; i++){
             graph.put(i, new ArrayList<>());
         }
 
         for (int i = 0; i < E; i++){
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
             graph.get(u).add(new Node(v, w));
         }
 
-        // init
-        distance = new int[V+1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[start] = 0;
-        pq.offer(new Node(start, 0));
 
-        // djikstra
-        while (!pq.isEmpty()){
-            Node curNode = pq.poll();
+        djikstra(K);
 
-            for (int i = 0; i < graph.get(curNode.idx).size(); i++){
-                Node nxtNode = graph.get(curNode.idx).get(i);
-
-                if (distance[nxtNode.idx] > curNode.cost + nxtNode.cost){
-                    distance[nxtNode.idx] = curNode.cost + nxtNode.cost;
-                    pq.offer(new Node(nxtNode.idx, distance[nxtNode.idx]));
-                }
-            }
-        }
-
-        // print
         for (int i = 1; i <= V; i++){
             if (distance[i] == Integer.MAX_VALUE){
                 System.out.println("INF");
             }
-            else {
+            else{
                 System.out.println(distance[i]);
             }
         }
+        br.close();
     }
 
     public static void main(String[] args) throws IOException {
