160fa5205058f8a6f3aaccd1501df50a3eebad15
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-04-10 23:58:21 +0900

    [Silver I] Title: 나이트의 이동, Time: 312 ms, Memory: 112184 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     static int T, l;
     static int[][] map;
-    static int[][] moveCount;
     static int[] dx = {1, 2, 2, 1, -1, -2,-2,-1};
     static int[] dy = {2, 1,-1,-2, -2, -1, 1, 2};
     static Queue<Point> q;
@@ -26,14 +25,9 @@ public class Main {
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
                 if (0 <= nx && nx < l && 0 <= ny && ny < l){
-                    if (moveCount[nx][ny] == -1){
+                    if (map[nx][ny] == -1){
                         q.offer(new Point(nx, ny));
-                        moveCount[nx][ny] = moveCount[p.x][p.y] + 1;
-
-                        if (nx == end_x && ny == end_y){
-                            System.out.println(moveCount[nx][ny]);
-                            return;
-                        }
+                        map[nx][ny] = map[p.x][p.y] + 1;
                     }
                 }
             }
@@ -49,25 +43,22 @@ public class Main {
             l = Integer.parseInt(br.readLine());
             q = new LinkedList<>();
             map = new int[l][l];
-            moveCount = new int[l][l];
             for (int i = 0; i < l; i++){
-                Arrays.fill(moveCount[i], -1);
+                Arrays.fill(map[i], -1);
             }
 
             st = new StringTokenizer(br.readLine());
             int start_x = Integer.parseInt(st.nextToken());
             int start_y = Integer.parseInt(st.nextToken());
             q.offer(new Point(start_x, start_y));
-            moveCount[start_x][start_y] = 0;
+            map[start_x][start_y] = 0;
 
             st = new StringTokenizer(br.readLine());
             end_x = Integer.parseInt(st.nextToken());
             end_y = Integer.parseInt(st.nextToken());
 
-            if (start_x == end_x && start_y == end_y){
-                System.out.println(0);
-            }
-            else {bfs();};
+            bfs();
+            System.out.println(map[end_x][end_y]);
         }
 
         br.close();
