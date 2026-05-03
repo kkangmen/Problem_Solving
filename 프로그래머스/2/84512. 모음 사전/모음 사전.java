e3e3629172ec9c24955b6e1e217a2f5603332096
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-05-03 23:02:58 +0900

    [level 2] Title: 모음 사전, Time: 25.11 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    private static List<String> dictionary = new LinkedList<>();
+    private static String[] vowel = {"A", "E", "I", "O", "U"};
+    
+    public void dfs(String s, int depth){
+        dictionary.add(s);
+        if (s.length() == 5){
+            return;
+        }
+        
+        for (int i = 0; i < 5; i++){
+            dfs(s + vowel[i], depth+1);
+        }
+    }
+    
+    public int solution(String word) {
+        int answer = 0;        
+        dfs("", 0);
+        
+        for (int i = 0; i < dictionary.size(); i++){
+            if (word.equals(dictionary.get(i))){
+                answer = i;
+            }
+        }
+        return answer;
+    }
+}
