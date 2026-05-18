0ce9a30a65a358263e0e1197145b0871562cc7d8
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-05-18 17:25:47 +0900

    [level 3] Title: 섬 연결하기, Time: 0.27 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -3,77 +3,67 @@ import java.util.*;
 class Solution {
     
     class Node {
-        int startNode;
-        int endNode;
+        int start;
+        int end;
         int cost;
-        Node (int startNode, int endNode, int cost){
-            this.startNode = startNode;
-            this.endNode = endNode;
+        Node (int start, int end, int cost){
+            this.start = start;
+            this.end = end;
             this.cost = cost;
         }
     }
     
-    // 수정 2: static 제거 (테스트 케이스 간 간섭 방지)
-    PriorityQueue<Node> pq = new PriorityQueue<>((Node o1, Node o2) -> {
-        return Integer.compare(o1.cost, o2.cost);
+    int[] parent;
+    PriorityQueue<Node> pq = new PriorityQueue<>((Node p1, Node p2) -> {
+        return Integer.compare(p1.cost, p2.cost);
     });
     
-    // 수정 2: static 제거
-    int[] parent;
+    // union 합치기
+    public void union(int mother, int baby){
+        mother = findParent(mother);
+        baby = findParent(baby);
+        
+        if (mother < baby){
+            parent[baby] = mother;
+        } else {
+            parent[mother] = baby;
+        }
+    }
     
+    // 부모 찾기
     public int findParent(int nodeNum){
         if (parent[nodeNum] == nodeNum){
             return nodeNum;
         }
-        // 수정 3: 경로 압축(Path Compression) 적용
-        return parent[nodeNum] = findParent(parent[nodeNum]);
+        return findParent(parent[nodeNum]);
     }
     
-    public boolean union(int node1, int node2){
-        // 수정 1: 각 노드의 최상위 부모(root)를 먼저 찾습니다.
-        int root1 = findParent(node1);
-        int root2 = findParent(node2);
-        
-        // 부모가 같다면 이미 연결된 상태
-        if (root1 == root2){
-            return true;
+    public int getMinimumCost(int answer){
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            
+            // 부모가 다르면 간선 추가.
+            if (findParent(node.start) != findParent(node.end)){
+                answer += node.cost;
+                union(node.start, node.end);
+            }
         }
-        
-        // 수정 1: 노드 자신이 아닌, '루트 노드'끼리 연결합니다.
-        if (root1 < root2){
-            parent[root2] = root1;
-        } else {
-            parent[root1] = root2;
-        }
-        return false;
+        return answer;
     }
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        
-        // 만약을 대비해 pq 초기화 (인스턴스 변수라도 확실히 비워주는 것이 좋습니다)
-        pq.clear();
-        
-        // 부모 노드 초기화
         parent = new int[n];
+        
         for (int i = 0; i < n; i++){
             parent[i] = i;
         }
         
-        // pq에 값 넣기
-        for (int[] nodeInfo : costs){
-            pq.offer(new Node(nodeInfo[0], nodeInfo[1], nodeInfo[2]));
+        for (int[] cost: costs){
+            pq.add(new Node(cost[0], cost[1], cost[2]));
         }
         
-        while (!pq.isEmpty()){
-            Node info = pq.poll();
-            
-            // 부모 노드가 다르면 간선 추가
-            if (!union(info.startNode, info.endNode)){
-                answer += info.cost;
-            }
-        }
-        
+        answer = getMinimumCost(answer);
         return answer;
     }
 }
