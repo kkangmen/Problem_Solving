bfa6b6ccfb57f97964be04e04e99b23fa82160ab
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-05-24 23:47:12 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.67 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -2,39 +2,37 @@ import java.util.*;
 
 class Solution {
     
-    Queue<Integer> serverTTL = new LinkedList<>();
+    Queue<Integer> endTime = new LinkedList<>();
     
     public int solution(int[] players, int m, int k) {
-        int answer = 0;
-        
+        int cumulServerCnt = 0;
         int curServerCnt = 0;
         for (int i = 0; i < players.length; i++){
-            // System.out.println("i: " + i);
-            
-            // 서버 만료 시간이 도달하였을 경우 제거.
-            while (!serverTTL.isEmpty() && serverTTL.peek() == i){
-                // System.out.println("서버 만료");
+            // System.out.println("i = " + i);
+            // 서버 시간이 다되었다면
+            while (!endTime.isEmpty() && endTime.peek() == i){
+                endTime.poll();
                 curServerCnt -= 1;
-                serverTTL.poll();
             }
             
+            // 서버가 더 필요할 경우
             int needServerCnt = players[i]/m;
-            // 필요 서버 수가 현재 서버 수보다 많을 경우
-            if (curServerCnt < needServerCnt){
-                answer += needServerCnt - curServerCnt;
-                
-                // 증설된 서버 수 만큼 서버 만료 시간 큐에 삽입
-                for (int count = 0; count < needServerCnt-curServerCnt; count++){
-                    serverTTL.offer(i+k);
+            if (needServerCnt > curServerCnt){
+                // 증설 횟수만큼 서버 추가
+                int plusServer = (needServerCnt - curServerCnt);
+                for (int count = 0; count < plusServer; count++){
+                    endTime.offer(i+k);
                 }
                 
-                curServerCnt += needServerCnt - curServerCnt;
+                // 우선 누적 증설횟수 더해줌.
+                cumulServerCnt += plusServer;
                 
-                
+                curServerCnt = needServerCnt;
             }
-            // System.out.println("증설된 서버의 수: " + curServerCnt);
-            // System.out.println("증설 횟수: " + answer);            
+            // System.out.println("증설된 서버의 수 = "+  curServerCnt);
+            // System.out.println("증설 횟수 = "+ cumulServerCnt);
         }
-        return answer;
+        
+        return cumulServerCnt;
     }
 }
