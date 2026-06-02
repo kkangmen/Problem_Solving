456fa58ec95e5707a95fa88b1ac4d5c26fecfc96
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-02 23:55:29 +0900

    [level 3] Title: N으로 표현, Time: 14.05 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 class Solution {
     
-    public static List<Set<Integer>> dp = new ArrayList<>();
+    static List<Set<Integer>> dp = new ArrayList<>();
+    
     public int solution(int N, int number) {
+        int answer = 0;
         
         for (int i = 0; i < 9; i++){
             dp.add(new HashSet<>());
@@ -12,34 +14,31 @@ class Solution {
         dp.get(1).add(N);
         
         for (int i = 2; i < 9; i++){
-            
-            Set<Integer> now = dp.get(i);
-            
+            Set<Integer> curSet = dp.get(i);
             for (int j = 1; j < i; j++){
                 Set<Integer> set1 = dp.get(j);
                 Set<Integer> set2 = dp.get(i-j);
                 
                 for (int num1 : set1){
-                    for (int num2 : set2){
-                        now.add(num1 + num2);
-                        now.add(num1 - num2);
-                        now.add(num1 * num2);
+                    for (int num2: set2){
+                        curSet.add(num1+num2);
+                        curSet.add(num1-num2);
+                        curSet.add(num1*num2);
                         
                         if (num1 != 0 && num2 != 0){
-                            now.add(num1/num2);
+                            curSet.add(num1/num2);
                         }
                     }
                 }
             }
-            now.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            curSet.add(Integer.parseInt(String.valueOf(N).repeat(i)));
         }
         
-        for (Set<Integer> sub: dp){
-            if (sub.contains(number)){
-                return dp.indexOf(sub);
+        for (Set s : dp){
+            if (s.contains(number)){
+                return dp.indexOf(s);
             }
         }
-        
         return -1;
     }
 }
