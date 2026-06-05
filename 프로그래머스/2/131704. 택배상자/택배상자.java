05405e007684cff538f8b39f0038cc91b457d15c
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-05 15:29:34 +0900

    [level 2] Title: 택배상자, Time: 106.25 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     
-    Stack<Integer> s = new Stack<>();
+    static Stack<Integer> belt = new Stack<>();
     
     public int solution(int[] order) {
         int answer = 0;
-            
+        
         for (int i = 1; i <= order.length; i++){
-            s.push(i);
+            belt.push(i);
             
-            while (!s.isEmpty() && s.peek() == order[answer]){
+            while (!belt.isEmpty() && belt.peek() == order[answer]){
                 answer++;
-                s.pop();
+                belt.pop();
             }
         }
+        
         return answer;
     }
 }
