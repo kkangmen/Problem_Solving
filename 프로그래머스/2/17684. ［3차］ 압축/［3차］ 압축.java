4d79eb454899b77ff49bcef509afe00ea6ac2a64
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-09 23:28:57 +0900

    [level 2] Title: [3차] 압축, Time: 8.53 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 class Solution {
     
-    static Map<String, Integer> dictionary = new HashMap<>();
+    static HashMap<String, Integer> dictionary = new HashMap<>();
     static int index = 0;
     
     public List<Integer> solution(String msg) {
         List<Integer> answer = new ArrayList<>();
         
         for (char ch = 'A'; ch <= 'Z'; ch++){
-            dictionary.put(String.valueOf(ch), ++index);
+            dictionary.put(String.valueOf(ch), ++index);        
         }
         
-        for (int i = 0; i < msg.length(); ){
+        String word = "";
+        int prevIndex = 0;
+        for (int i = 0; i < msg.length(); i++){
+            word += msg.charAt(i);
             
-            String word = String.valueOf(msg.charAt(i));
-            int nxtIdx = i+1;
-            
-            while (nxtIdx < msg.length() && dictionary.containsKey(word + msg.charAt(nxtIdx))){
-                word += msg.charAt(nxtIdx);
-                nxtIdx++;
+            if(!dictionary.containsKey(word)){
+                dictionary.put(word, ++index);
+                word = "";
+                i--;
+                answer.add(prevIndex);
+            } else{
+                prevIndex = dictionary.get(word);
             }
-            
-            answer.add(dictionary.get(word));
-            
-            if (nxtIdx < msg.length()){
-                dictionary.put(word + msg.charAt(nxtIdx), ++index);
-            }
-            
-            i = nxtIdx;
         }
+        
+        answer.add(dictionary.get(word));
+        
         return answer;
     }
 }
