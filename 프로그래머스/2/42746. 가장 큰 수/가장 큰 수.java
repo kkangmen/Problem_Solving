758e1229a9d206a9cdd0c0d11989ab689f5a26ed
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-12 17:47:13 +0900

    [level 2] Title: 가장 큰 수, Time: 94.35 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
 class Solution {
+    
+    List<String> strNumList = new ArrayList<>();
+    
     public String solution(int[] numbers) {
-        StringBuilder sb = new StringBuilder();
-        
-        List<String> numberList = new ArrayList<>();
+        StringBuilder answer = new StringBuilder();
         
         for (int i : numbers){
-            numberList.add(String.valueOf(i));
+            strNumList.add(String.valueOf(i));
         }
         
-        Collections.sort(numberList, (String s1, String s2) -> {
-           return Integer.parseInt(s2+s1) - Integer.parseInt(s1+s2); 
+        Collections.sort(strNumList, (o1, o2) -> {
+           return (o2+o1).compareTo(o1+o2);
         });
         
-        for (int i = 0; i < numberList.size(); i++){
-            if (numberList.get(0).equals("0")){
-                return "0";
-            }
-            sb.append(numberList.get(i));
+        if (strNumList.get(0).equals("0")){
+            return "0";
         }
         
+        for (String s : strNumList){        
+            answer.append(s);
+        }
         
-        return sb.toString();
+        return answer.toString();
     }
 }
