7252214f260bc23976a043522769592391fa8312
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-17 18:39:10 +0900

    [level 3] Title: N으로 표현, Time: 13.27 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,41 +2,42 @@ import java.util.*;
 
 class Solution {
     
-    static List<Set<Integer>> dp = new ArrayList<>();
+    List<Set<Integer>> dp = new ArrayList<>();
     
     public int solution(int N, int number) {
         int answer = 0;
         
-        for (int i = 0; i < 9; i++){
+        for (int i = 0; i < 8; i++){
             dp.add(new HashSet<>());
         }
         
-        dp.get(1).add(N);
+        // 초기화
+        dp.get(0).add(N);
         
-        for (int i = 2; i < 9; i++){
+        for (int i = 1; i < 8; i++){
             Set<Integer> curSet = dp.get(i);
-            for (int j = 1; j < i; j++){
+            
+            for (int j = 0; j < i; j++){
                 Set<Integer> set1 = dp.get(j);
-                Set<Integer> set2 = dp.get(i-j);
+                Set<Integer> set2 = dp.get(i-1-j);
                 
                 for (int num1 : set1){
                     for (int num2: set2){
                         curSet.add(num1+num2);
                         curSet.add(num1-num2);
                         curSet.add(num1*num2);
-                        
                         if (num1 != 0 && num2 != 0){
                             curSet.add(num1/num2);
                         }
                     }
                 }
             }
-            curSet.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            curSet.add(Integer.parseInt(String.valueOf(N).repeat(i+1)));
         }
         
-        for (Set s : dp){
-            if (s.contains(number)){
-                return dp.indexOf(s);
+        for (Set set: dp){
+            if (set.contains(number)){
+                return dp.indexOf(set)+1;
             }
         }
         return -1;
