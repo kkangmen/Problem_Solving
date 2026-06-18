7b362b5f4dc47cfcde80f579912abefef38f7638
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-18 10:29:37 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 15.90 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -2,40 +2,35 @@ import java.util.*;
 
 class Solution {
     
-    static int[] answer = new int[2];
-    
-    public boolean chkQuad(int[][] arr, int x, int y, int length){
+    public void zip(int x, int y, int length, int[][] arr, int[] answer){
+        boolean flag = true;
+        int num = arr[x][y];
+        
+        // System.out.println(x + " " + y + " " + length);
+        
         for (int i = x; i < x+length; i++){
             for (int j = y; j < y+length; j++){
-                if (arr[i][j] != arr[x][y]){
-                    return false;
+                if (arr[i][j] != num){
+                    flag = false;
                 }
             }
         }
         
-        return true;
-    }
-    
-    public void srcQuad(int[][] arr, int x, int y, int length){
-        if (chkQuad(arr, x, y, length)){
-            if (arr[x][y] == 0){
-                answer[0]++;
-            } else {
-                answer[1]++;
-            }
+        if (flag){
+            answer[num] += 1;
             return;
+        } else {
+            zip(x, y, length/2, arr, answer);
+            zip(x, y+length/2, length/2, arr, answer);
+            zip(x+length/2, y, length/2, arr, answer);
+            zip(x+length/2, y+length/2, length/2, arr, answer);
         }
-        
-        srcQuad(arr, x, y, length/2);
-        srcQuad(arr, x+length/2, y, length/2);
-        srcQuad(arr, x, y+length/2, length/2);
-        srcQuad(arr, x+length/2, y+length/2, length/2);
     }
     
     public int[] solution(int[][] arr) {
+        int[] answer = new int[2];
         
-        srcQuad(arr, 0, 0, arr.length);
-        
+        zip(0, 0, arr.length, arr, answer);
         return answer;
     }
 }
