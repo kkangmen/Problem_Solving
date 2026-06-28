d2afad47decb4295ef6cd06f459788b66320466d
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-28 15:21:25 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 17.34 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     
-    Queue<Integer> bridge = new LinkedList<>();
-    Queue<Integer> q = new LinkedList<>();
+    Queue<Integer> waiting = new LinkedList<>();
+    Queue<Integer> bridge_queue = new LinkedList<>();
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int time = 0;
         
+        // 초기화
         for (int i : truck_weights){
-            q.offer(i);
+            waiting.offer(i);
         }
+        int totalWeight = 0;
         
-        int tBW = 0;
-        while (!q.isEmpty()){
+        while (!waiting.isEmpty()){
             
-            // brideg에 자리가 없으면 빼고 시작.
-            if (bridge.size() == bridge_length){
-                tBW -= bridge.poll();
+            if (bridge_queue.size() == bridge_length){
+                totalWeight -= bridge_queue.poll();
             }
             
-            if (tBW + q.peek() <= weight){
-                bridge.offer(q.peek());
-                tBW += q.poll();
-                time++;
-            } else { // 자리가 있는데 무게 초과일 경우
-                bridge.offer(0);
-                time++;
+            if (totalWeight + waiting.peek() <= weight){
+                int cur = waiting.poll();
+                bridge_queue.offer(cur);
+                totalWeight += cur;
+            } else {
+                bridge_queue.offer(0);
             }
+            time++;
         }
         
         return time + bridge_length;
