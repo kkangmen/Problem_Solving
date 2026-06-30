41730b506926abe9ce5123739ee8abefb92e2abb
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-06-30 11:49:16 +0900

    [level 2] Title: 삼각 달팽이, Time: 15.43 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -4,31 +4,29 @@ class Solution {
     public List<Integer> solution(int n) {
         List<Integer> answer = new ArrayList<>();
         
-        int[][] triangle = new int[n][n];
-        
+        int[][] arr = new int[n][n];
         int num = 1;
-        int x = -1;
-        int y = 0;
-        for (int i = 0; i < n; i++){
-            for (int j = i; j < n; j++){
-                if (i % 3 == 0){
-                    x++;
-                } else if (i % 3 == 1){
-                    y++;
+        int row = -1; int col = 0;
+        
+        for (int i = 1; i <= n; i++){
+            for (int j = 0; j < n+1-i; j++){
+                if (i % 3 == 1){
+                    row++;
+                } else if (i % 3 == 2){
+                    col++;
                 } else {
-                    x--;
-                    y--;
+                    row--;
+                    col--;
                 }
-                triangle[x][y] = num++;
+                arr[row][col] = num++;
             }
         }
         
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                if (triangle[i][j] == 0){
-                    continue;
+                if (arr[i][j] != 0){
+                    answer.add(arr[i][j]);
                 }
-                answer.add(triangle[i][j]);
             }
         }
         return answer;
