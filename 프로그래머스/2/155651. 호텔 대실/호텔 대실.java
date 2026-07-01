96c96876b82676a24b550685a0fc0d6d2a4adcc5
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-07-01 11:11:44 +0900

    [level 2] Title: 호텔 대실, Time: 60.13 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -3,47 +3,40 @@ import java.util.*;
 class Solution {
     boolean[] isChecked;
     
-    public int timeConvert(String s){
-        String[] time = s.split(":");
-        
-        int hour = Integer.parseInt(time[0]);
-        int min = Integer.parseInt(time[1]);
-        
-        return hour*60+min;
+    public int timeConvert(String time){
+        String[] arr = time.split(":");
+        int hour = Integer.parseInt(arr[0]);
+        int min = Integer.parseInt(arr[1]);
+        return hour*60 + min;
     }
     
     public void bfs(int index, String[][] book_time){
         isChecked[index] = true;
         int endTime = timeConvert(book_time[index][1]);
-        for (int i = index+1; i < isChecked.length; i++){
-            int startTime = timeConvert(book_time[i][0]);
+        
+        for (int i = index+1; i < book_time.length; i++){
+            int nxtStartTime = timeConvert(book_time[i][0]);
             
-            if (!isChecked[i] && endTime + 10 <= startTime){
-                isChecked[i] = true;
+            if ((endTime + 10 <= nxtStartTime) && (!isChecked[i])){
                 endTime = timeConvert(book_time[i][1]);
+                isChecked[i] = true;
             }
         }
     }
-    
     public int solution(String[][] book_time) {
         int answer = 0;
         
-        isChecked = new boolean[book_time.length];
         Arrays.sort(book_time, (o1, o2) -> {
            return o1[0].compareTo(o2[0]); 
         });
+        isChecked = new boolean[book_time.length];
         
-        
-        for (int i = 0; i < book_time.length; i++){
+        for (int i = 0; i < isChecked.length; i++){
             if (!isChecked[i]){
-                answer++;
                 bfs(i, book_time);
-            }    
+                answer++;
+            }
         }
-        
-        // for (String[] s : book_time){
-        //     System.out.println(s[0] + " " + s[1]);
-        // }
         return answer;
     }
 }
