5a368f4c792e354df3af3430fbbc6cdf3fb2f2a1
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-07-02 12:21:56 +0900

    [level 2] Title: 배달, Time: 3.53 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,53 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    class Node {
-        int dest;
+    class Point {
+        int idx;
         int cost;
-        Node (int dest, int cost){
-            this.dest = dest;
+        Point(int idx, int cost){
+            this.idx = idx;
             this.cost = cost;
         }
     }
     
-    Map<Integer, List<Node>> graph = new HashMap<>();
+    Map<Integer, List<Point>> graph = new HashMap();
     int[] distance;
-    PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
+    Queue<Point> pq = new PriorityQueue<>((o1, o2) -> {
         return o1.cost - o2.cost;
     });
     
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-
+        
         // 초기화
-        distance = new int[N+1];
         for (int i = 0; i <= N; i++){
             graph.put(i, new ArrayList<>());
         }
-        for (int[] i : road){
-            graph.get(i[0]).add(new Node(i[1], i[2]));
-            graph.get(i[1]).add(new Node(i[0], i[2]));
-        }
+        distance = new int[N+1];
+        
         Arrays.fill(distance, Integer.MAX_VALUE);
+        for (int[] i : road){
+            graph.get(i[0]).add(new Point(i[1], i[2]));
+            graph.get(i[1]).add(new Point(i[0], i[2]));
+        }
         
         // 다익스트라 시작
         distance[1] = 0;
-        pq.offer(new Node(1, 0));
+        pq.offer(new Point(1, 0));
         
         while (!pq.isEmpty()){
-            Node curNode = pq.poll();
-            
-            for (int i = 0; i < graph.get(curNode.dest).size(); i++){
-                Node nxtNode = graph.get(curNode.dest).get(i);
+            Point curP = pq.poll();
+            for (int i = 0; i < graph.get(curP.idx).size(); i++){
+                Point nxtP = graph.get(curP.idx).get(i);
                 
-                if (distance[nxtNode.dest] > distance[curNode.dest] + nxtNode.cost){
-                    distance[nxtNode.dest] = distance[curNode.dest] + nxtNode.cost;
-                    pq.offer(new Node(nxtNode.dest, distance[nxtNode.dest]));
+                if (distance[nxtP.idx] > distance[curP.idx] + nxtP.cost){
+                    distance[nxtP.idx] = distance[curP.idx] + nxtP.cost;
+                    pq.offer(nxtP);
                 }
             }
         }
         
-        for (int i = 1; i <= N; i++){
+        // 검증
+        for (int i = 1; i < N+1; i++){
             if (distance[i] <= K){
                 answer++;
             }
