692d433aad1f26e7bbc3677d1d948e4f5fe7ebf7
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-07-13 11:39:00 +0900

    [level 2] Title: 호텔 대실, Time: 10.16 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,42 +1,34 @@
 import java.util.*;
 
 class Solution {
-    boolean[] isChecked;
-    
-    public int timeConvert(String time){
-        String[] arr = time.split(":");
-        int hour = Integer.parseInt(arr[0]);
-        int min = Integer.parseInt(arr[1]);
+        
+    public int convertTime(String time){
+        int hour = Integer.parseInt(time.split(":")[0]);
+        int min = Integer.parseInt(time.split(":")[1]);
+        
         return hour*60 + min;
     }
     
-    public void bfs(int index, String[][] book_time){
-        isChecked[index] = true;
-        int endTime = timeConvert(book_time[index][1]);
-        
-        for (int i = index+1; i < book_time.length; i++){
-            int nxtStartTime = timeConvert(book_time[i][0]);
-            
-            if ((endTime + 10 <= nxtStartTime) && (!isChecked[i])){
-                endTime = timeConvert(book_time[i][1]);
-                isChecked[i] = true;
-            }
-        }
-    }
     public int solution(String[][] book_time) {
         int answer = 0;
         
-        Arrays.sort(book_time, (o1, o2) -> {
-           return o1[0].compareTo(o2[0]); 
-        });
-        isChecked = new boolean[book_time.length];
+        Queue<Integer> pq = new PriorityQueue<>();
         
-        for (int i = 0; i < isChecked.length; i++){
-            if (!isChecked[i]){
-                bfs(i, book_time);
-                answer++;
+        Arrays.sort(book_time, (s1, s2) -> {
+            return s1[0].compareTo(s2[0]);
+        });
+            
+        for (String[] s : book_time){
+            int startTime = convertTime(s[0]);
+            int endTime = convertTime(s[1]);
+            
+            if (!pq.isEmpty() && pq.peek() <= startTime){
+                pq.poll();
             }
+            
+            pq.offer(endTime+10);
         }
-        return answer;
+        
+        return pq.size();
     }
 }
