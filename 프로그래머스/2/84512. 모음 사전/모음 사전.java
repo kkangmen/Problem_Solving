205bdfbe7c57d896a9704f03dbcdcb386928b35f
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-07-13 12:20:02 +0900

    [level 2] Title: 모음 사전, Time: 7.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,34 +2,26 @@ import java.util.*;
 
 class Solution {
     
-    static String[] vowels = {"A", "E", "I", "O", "U"};
-    static String[] words = new String[5];
-    static List<String> wordsOrder = new ArrayList<>();
+    String[] words = {"A", "E", "I", "O", "U"};
+    List<String> wordList = new LinkedList<>();
     
-    public void bF(int count){
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < count; i++){
-            sb.append(words[i]);
-        }
-        wordsOrder.add(sb.toString());
-        
-        if (count == 5){
+    public void bTracking(String curWord,int index){
+        if (curWord.length() == 6){
             return;
         }
         
+        // System.out.println(curWord);
+        wordList.add(curWord);
+        
         for (int i = 0; i < 5; i++){
-            words[count] = vowels[i];
-            bF(count+1);
+            bTracking(curWord + words[i], i);
         }
     }
     
-    public int solution(String word) {    
+    public int solution(String word) {
         int answer = 0;
-        
-        bF(0);
-        
-        answer = wordsOrder.indexOf(word);
-        return answer;
+    
+        bTracking("", 0);
+        return wordList.indexOf(word);
     }
 }
