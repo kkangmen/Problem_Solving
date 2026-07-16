84c43fcdab932c976cef2f38ffc1da8d9d84f7bd
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-07-16 17:48:16 +0900

    [level 2] Title: [3차] 압축, Time: 4.39 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,31 +2,30 @@ import java.util.*;
 
 class Solution {
     
-    static HashMap<String, Integer> dictionary = new HashMap<>();
-    static int index = 0;
+    Map<String, Integer> dictionary = new HashMap<>();
     
     public List<Integer> solution(String msg) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new LinkedList<>();
         
+        int index = 1;
         for (char ch = 'A'; ch <= 'Z'; ch++){
-            dictionary.put(String.valueOf(ch), ++index);        
+            dictionary.put(String.valueOf(ch), index++);        
         }
         
         String word = "";
-        int prevIndex = 0;
+        int wordIndex = 0;
         for (int i = 0; i < msg.length(); i++){
-            word += msg.charAt(i);
+            word = word + String.valueOf(msg.charAt(i));
             
-            if(!dictionary.containsKey(word)){
-                dictionary.put(word, ++index);
+            if (dictionary.containsKey(word)){
+                wordIndex = dictionary.get(word);
+            } else {
+                dictionary.put(word, index++);
+                answer.add(wordIndex);
                 word = "";
                 i--;
-                answer.add(prevIndex);
-            } else{
-                prevIndex = dictionary.get(word);
             }
         }
-        
         answer.add(dictionary.get(word));
         
         return answer;
