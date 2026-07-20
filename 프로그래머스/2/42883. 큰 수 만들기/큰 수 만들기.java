5a3f2ad53dce770778ed7b0ce116c7343a7c8d06
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-07-20 11:09:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 3834.39 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -4,15 +4,19 @@ class Solution {
     public String solution(String number, int k) {
         StringBuilder answer = new StringBuilder();
         
-        int maxIdx = 0;
+        int maxIndex = 0;
         for (int i = 0; i < number.length()-k; i++){
+            
             char maxChar = '0'-1;
-            for (int j = maxIdx; j <= k+i; j++){
-                if (maxChar < number.charAt(j)){
-                    maxChar = number.charAt(j);
-                    maxIdx = j+1;
+            for (int j = maxIndex; j < k+1+i; j++){
+                char curChar = number.charAt(j);
+                
+                if (maxChar < curChar){
+                    maxChar = curChar;
+                    maxIndex = j;
                 }
             }
+            maxIndex += 1;
             answer.append(String.valueOf(maxChar));
         }
         return answer.toString();
