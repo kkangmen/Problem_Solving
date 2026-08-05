1155254ad5e7455455b23906524a7bf27d3f981f
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-05 11:52:39 +0900

    [level 3] Title: 야근 지수, Time: 64.46 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 65.92 ms
+메모리: 73.8 MB, 시간: 64.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 17일 12:08:40
+2026년 08월 05일 11:52:36
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -6,20 +6,21 @@ class Solution {
     
     public long solution(int n, int[] works) {
         long answer = 0;
-        
+            
         for (int work : works){
             pq.offer(work);
         }
         
-        while (n > 0 && pq.peek() != 0){
-            pq.offer(pq.poll() - 1);
+        while (n > 0){
+            pq.offer(pq.poll()-1);
             n--;
         }
         
-        for (int work : pq){
-            answer += Math.pow(work, 2);
+        for (int i : pq){
+            if (i > 0){
+                answer += Math.pow(i, 2);
+            }
         }
-        
         return answer;
     }
 }
