d068e1e146fa4b2fca633bcde55c9b4bb1a6f16a
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-06 12:18:50 +0900

    [level 3] Title: 이중우선순위큐, Time: 608.69 ms, Memory: 203 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,53 +1,54 @@
 import java.util.*;
 
 class Solution {
-    Queue<Integer> pq = new PriorityQueue<>();
-    Queue<Integer> pqReverse = new PriorityQueue<>(Collections.reverseOrder());
     
-    public int removeMaximum(Queue<Integer> pq){
+    Queue<Integer> pq = new PriorityQueue<>();
+    
+    public int removeBiggest(){
+        Stack<Integer> s = new Stack<>();
         while (!pq.isEmpty()){
-            pqReverse.offer(pq.poll());
+            s.push(pq.poll());
         }
-        
-        int num = pqReverse.poll();
-        while (!pqReverse.isEmpty()){
-            pq.offer(pqReverse.poll());
+        int num = s.pop();
+        while (!s.isEmpty()){
+            pq.offer(s.pop());
         }
-        
         return num;
     }
     
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
         
-        for (String operation : operations){
-            String order = operation.split(" ")[0];
-            int num = Integer.parseInt(operation.split(" ")[1]);
+        for (String s : operations){
+            String order = s.split(" ")[0];
+            int number = Integer.parseInt(s.split(" ")[1]);
             
             if (order.equals("I")){
-                pq.offer(num);
-            } else { // D인 경우
-                if (pq.isEmpty()){ // 큐가 비어있으면 무시
+                pq.offer(number);
+            } else {
+                // 우선순위 큐가 비어있으면 무시
+                if (pq.isEmpty()){
                     continue;
                 }
-                if (num == 1){ // 최댓값 삭제
-                    removeMaximum(pq);
-                } else { // 최솟값 삭제
+                
+                if (number == 1){
+                    removeBiggest();
+                } else {
                     pq.poll();
                 }
             }
-        }
+        }    
         
         if (pq.isEmpty()){
             answer[0] = 0;
             answer[1] = 0;
-        } else if (pq.size() == 1){
-            int num = pq.poll();
-            answer[0] = num;
-            answer[1] = num;
         } else {
-            answer[0] = removeMaximum(pq);
-            answer[1] = pq.poll();
+            answer[0] = removeBiggest();
+            if (pq.isEmpty()){
+                answer[1] = answer[0];
+            } else {
+                answer[1] = pq.poll();                
+            }
         }
         return answer;
     }
