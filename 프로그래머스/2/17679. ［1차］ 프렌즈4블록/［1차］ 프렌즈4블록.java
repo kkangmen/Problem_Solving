eeccd0ddba248c717cdbc906074e2286160f525c
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-06 11:44:43 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 13.40 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -3,9 +3,10 @@ import java.util.*;
 class Solution {
     
     String[][] map;
-    int[][] changed;
+    boolean[][] isChanged;
+    int answer;
     
-    public void changeLocate(){
+    public void changePosition(){
         for (int j = 0; j < map[0].length; j++){
             for (int i = map.length-1; i >= 0; i--){
                 if (map[i][j].equals("-")){
@@ -21,36 +22,33 @@ class Solution {
         }
     }
     
-    public int countOne(){
-        int count = 0;
-        for (int i = 0; i < changed.length; i++){
-            for (int j = 0; j < changed[0].length; j++){
-                if (changed[i][j] == 1){
+    public void countGone(){
+        for (int i = 0; i < isChanged.length; i++){
+            for (int j = 0; j < isChanged[i].length; j++){
+                if (isChanged[i][j]){
                     map[i][j] = "-";
-                    count++;
+                    answer++;
                 }
             }
         }
-        return count;
     }
     
-    public boolean hasToChange(){
+    public boolean checkMap(){
         boolean flag = false;
         for (int i = 0; i < map.length-1; i++){
-            for (int j = 0; j < map[0].length-1; j++){
+            for (int j = 0; j < map[i].length-1; j++){
                 String s = map[i][j];
                 if (s.equals("-")){
                     continue;
                 }
-                
-                if (map[i][j+1].equals(s) &&
-                   map[i+1][j].equals(s) &&
-                   map[i+1][j+1].equals(s)){
-                    flag = true;
-                    changed[i][j] = 1;
-                    changed[i][j+1] = 1;
-                    changed[i+1][j] = 1;
-                    changed[i+1][j+1] = 1;
+                if (s.equals(map[i+1][j]) 
+                   && s.equals(map[i][j+1])
+                   && s.equals(map[i+1][j+1])){
+                   flag = true;
+                    isChanged[i][j] = true;
+                    isChanged[i+1][j] = true;
+                    isChanged[i][j+1] = true;
+                    isChanged[i+1][j+1] = true;
                 }
             }
         }
@@ -58,11 +56,12 @@ class Solution {
     }
     
     public int solution(int m, int n, String[] board) {
-        int answer = 0;
+        answer = 0;
         
-        // 초기화
-        map = new String[board.length][board[0].length()];    
-        for (int i = 0; i < board.length; i++){
+        map = new String[m][n];
+        isChanged = new boolean[m][n];
+        
+        for (int i = 0; i < m; i++){
             String s = board[i];
             for (int j = 0; j < s.length(); j++){
                 map[i][j] = String.valueOf(s.charAt(j));
@@ -70,24 +69,33 @@ class Solution {
         }
         
         while (true){
+            // 없어지는 게 있나?
+            if(!checkMap()){
+                break;
+            }
             
-            changed = new int[board.length][board[0].length()];
+            // 없어진 개수 세기
+            countGone();
+            isChanged = new boolean[m][n];
             
-            // 더 이상 없어지는 게 없다면
-            if (!hasToChange()) break;
+            // System.out.println("없어지는 거는 -로 바꿈");
+            // for (int i = 0; i < m; i++){
+            //     for (int j = 0; j < n; j++){
+            //         System.out.print(map[i][j] + " ");
+            //     }
+            //     System.out.println();
+            // }
             
-            // 1 개수 세기
-            answer += countOne();
+            // 위치 바꾸기
+            changePosition();
             
-            // 자리 바꾸기
-            changeLocate();
-            
-//             for (int i = 0; i < map.length; i++){
-//                 for (int j = 0; j < map[0].length; j++){
-//                     System.out.print(map[i][j]);
-//                 }
-//                 System.out.println();
-//             }
+            // System.out.println("위치 바꾸기");
+            // for (int i = 0; i < m; i++){
+            //     for (int j = 0; j < n; j++){
+            //         System.out.print(map[i][j] + " ");
+            //     }
+            //     System.out.println();
+            // }
         }
         return answer;
     }
