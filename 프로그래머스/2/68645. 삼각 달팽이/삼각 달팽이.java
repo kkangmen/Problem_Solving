403bdac36be2fcee2648f881ca66a80b12011b1f
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-07 11:21:30 +0900

    [level 2] Title: 삼각 달팽이, Time: 23.58 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,31 +1,58 @@
 import java.util.*;
 
 class Solution {
+    
+    int[][] triangle;
+    int num = 1;
+    int row = -1;
+    int col = 0;
+    int number = 1;
+    
+    public void goDown(int count){
+        for (int i = 0; i < count; i++){
+            row += 1;
+            triangle[row][col] = number;
+            number++;
+        }
+    }
+    
+    public void goRight(int count){
+        for (int i = 0; i < count; i++){
+            col += 1;
+            triangle[row][col] = number;
+            number++;
+        }
+    }
+        
+    public void goDiag(int count){
+        for (int i = 0; i < count; i++){
+            row -= 1;
+            col -= 1;
+            triangle[row][col] = number;
+            number++;
+        }
+    }
+    
     public List<Integer> solution(int n) {
         List<Integer> answer = new ArrayList<>();
         
-        int[][] arr = new int[n][n];
-        int num = 1;
-        int row = -1; int col = 0;
+        triangle = new int[n][n];
         
-        for (int i = 1; i <= n; i++){
-            for (int j = 0; j < n+1-i; j++){
-                if (i % 3 == 1){
-                    row++;
-                } else if (i % 3 == 2){
-                    col++;
-                } else {
-                    row--;
-                    col--;
-                }
-                arr[row][col] = num++;
+        for (int i = 0; i < n; i++){
+            switch (i%3) {
+                case 0: goDown(n-i);
+                    break;
+                case 1: goRight(n-i);
+                    break;
+                case 2: goDiag(n-i);
+                    break;
             }
         }
         
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                if (arr[i][j] != 0){
-                    answer.add(arr[i][j]);
+                if (triangle[i][j] != 0){
+                    answer.add(triangle[i][j]);                    
                 }
             }
         }
