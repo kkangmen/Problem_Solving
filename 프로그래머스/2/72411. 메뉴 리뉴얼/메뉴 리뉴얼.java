6d91604cbc57b6fdff0f13163031d3583b7368f9
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-12 11:15:09 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 16.08 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -2,56 +2,47 @@ import java.util.*;
 
 class Solution {
     
-    public void bTracking(String curWord, String word, int index, int endCount, Map<String,Integer> dictionary){
-        if (curWord.length() == endCount){
-            // System.out.println(curWord);
-            dictionary.put(curWord, dictionary.getOrDefault(curWord, 0) + 1);
+    Map<String, Integer> map;
+    
+    public void dfs(String order, String curMenu, int idx, int max){
+        if (curMenu.length() == max){
+            map.put(curMenu, map.getOrDefault(curMenu, 0) + 1);
             return;
         }
         
-        for (int i = index; i < word.length(); i++){
-            bTracking(curWord + word.charAt(i), word, i+1, endCount, dictionary);
+        for (int i = idx+1; i < order.length(); i++){
+            dfs(order, curMenu + order.charAt(i), i, max);
         }
     }
     
-    public  List<String> solution(String[] orders, int[] course) {
-        List<String> answer = new ArrayList<>();
+    public List<String> solution(String[] orders, int[] course) {
+        List<String> answer = new LinkedList<>();
         
+        // 정렬
         for (int i = 0; i < orders.length; i++){
-            String s = orders[i];
-            char[] sArray = s.toCharArray();
-            
-            Arrays.sort(sArray);
-            
-            orders[i] = new String(sArray);
+            char[] charArray = orders[i].toCharArray();
+            Arrays.sort(charArray);
+            orders[i] = String.valueOf(charArray);
         }
         
-        for (int i = 0; i < course.length; i++){
-            int endCount = course[i]; // 2
+        for (int menuCnt : course){
+            map = new HashMap<>();
             
-            // 백트래킹 시작
-            Map<String, Integer> dictionary = new HashMap<>();
-            
-            for (int j = 0; j < orders.length; j++){
-                bTracking("", orders[j], 0, endCount, dictionary);
+            for (String order : orders){
+                dfs(order, "", -1, menuCnt);
             }
             
-            // 가장 많이 주문된 메뉴
-            int max = 0;
-            for (String menu : dictionary.keySet()){
-                int count = dictionary.get(menu);
-                if (max <= count){
-                    max = count;
+            // 최대 갯수 구하기
+            int max = 2;
+            for (String s : map.keySet()){
+                if (map.get(s) > max){
+                    max = map.get(s);
                 }
             }
             
-            // 2개 이상만 정답
-            if (max == 1){
-                continue;
-            }
-            for (String menu : dictionary.keySet()){
-                if (dictionary.get(menu) == max){
-                    answer.add(menu);
+            for (String s: map.keySet()){
+                if (map.get(s) == max){
+                    answer.add(s);
                 }
             }
         }
