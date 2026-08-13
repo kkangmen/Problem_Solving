5088de030e8a9d1868272ae4ea34f424ac2ead60
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-13 11:34:46 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.80 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,37 +2,35 @@ import java.util.*;
 
 class Solution {
     
-    String[][] graph;
+    String[][] map;
     int[][] distance;
     int[] dx = {0, 1, 0, -1};
     int[] dy = {1, 0, -1, 0};
-    Queue<int[]> queue = new LinkedList<>();
-    int row = 0;
-    int col = 0;
-    public void bfs(int[] start){
-        int x = start[0];
-        int y = start[1];
-        distance[x][y] = 0;
-        queue.offer(start);
+    int[] start;
+    int[] end;
+    int row = 0; int col = 0;
+    Queue<int[]> q = new LinkedList<>();
+    
+    public void bfs(){
+        q.offer(start);
+        distance[start[0]][start[1]] = 1;
         
-        while(!queue.isEmpty()){
-            int[] curNode = queue.poll();
+        while (!q.isEmpty()){
+            int[] curPoint = q.poll();
             
             for (int i = 0; i < 4; i++){
-                int nx = curNode[0];
-                int ny = curNode[1];
-                
-                // 장애물이 나타나거나 맵 끝까지 갈때까지
-                while ((0 <= nx+dx[i] && nx+dx[i] < row &&
-                      0 <= ny+dy[i] && ny+dy[i] < col) &&
-                    (!graph[nx+dx[i]][ny+dy[i]].equals("D"))){
+                int nx = curPoint[0];
+                int ny = curPoint[1];
+                // 다음 이동할 칸이 벽에 닿거나, D가 나올때까지 이동
+                while ((0 <= nx+dx[i] && nx+dx[i] < row && 0 <= ny+dy[i] && ny+dy[i] < col) 
+                    && (!map[nx+dx[i]][ny+dy[i]].equals("D"))){
                     nx += dx[i];
                     ny += dy[i];
                 }
                 
-                if (distance[nx][ny] == -1){
-                    queue.offer(new int[]{nx, ny});
-                    distance[nx][ny] = distance[curNode[0]][curNode[1]] + 1;
+                if (distance[nx][ny] == 0){
+                    distance[nx][ny] = distance[curPoint[0]][curPoint[1]] + 1;
+                    q.offer(new int[]{nx, ny});
                 }
             }
         }
@@ -40,44 +38,31 @@ class Solution {
     
     public int solution(String[] board) {
         int answer = 0;
-        int[] start = new int[2];
-        int[] goal = new int[2];
-        // 그래프 초기화
+        
+        // 초기화
         row = board.length;
         col = board[0].length();
-        graph = new String[row][col];
+        map = new String[row][col];
         distance = new int[row][col];
+        
         for (int i = 0; i < board.length; i++){
             String s = board[i];
             for (int j = 0; j < s.length(); j++){
-                graph[i][j] = String.valueOf(s.charAt(j));
-                if (graph[i][j].equals("R")){
+                map[i][j] = String.valueOf(s.charAt(j));
+                if (map[i][j].equals("R")){
                     start = new int[]{i, j};
-                } else if (graph[i][j].equals("G")){
-                    goal = new int[]{i, j};
+                }
+                if (map[i][j].equals("G")){
+                    end = new int[]{i, j};
                 }
             }
         }
-        for (int i = 0; i< row; i++){
-            Arrays.fill(distance[i], -1);
+        
+        bfs();
+        
+        if (distance[end[0]][end[1]] == 0){
+            return -1;
         }
-        // bfs 시작
-        bfs(start);
-        
-        // for (int i = 0; i < row; i++){
-        //     for (int j= 0; j < col; j++){
-        //         System.out.print(graph[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println(start[0] + " " +  start[1]);
-        
-        // for (int i = 0; i < row; i++){
-        //     for (int j = 0; j < col; j++){
-        //         System.out.print(distance[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        return distance[goal[0]][goal[1]];
+        return distance[end[0]][end[1]]-1;
     }
 }
