51a4e6769637535e5d2b1f14ccf9b5994a449d3e
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-14 15:01:53 +0900

    [level 2] Title: 호텔 대실, Time: 13.70 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,34 +1,38 @@
 import java.util.*;
 
 class Solution {
-        
-    public int convertTime(String time){
-        int hour = Integer.parseInt(time.split(":")[0]);
-        int min = Integer.parseInt(time.split(":")[1]);
-        
-        return hour*60 + min;
-    }
     
+    Queue<Integer> pq = new PriorityQueue<>();
+    
+    public int convertToTime(String s){
+        int hour = Integer.parseInt(s.split(":")[0]);
+        int min = Integer.parseInt(s.split(":")[1]);
+        
+        return min + hour*60;
+    }
     public int solution(String[][] book_time) {
         int answer = 0;
-        
-        Queue<Integer> pq = new PriorityQueue<>();
         
         Arrays.sort(book_time, (s1, s2) -> {
             return s1[0].compareTo(s2[0]);
         });
-            
-        for (String[] s : book_time){
-            int startTime = convertTime(s[0]);
-            int endTime = convertTime(s[1]);
-            
-            if (!pq.isEmpty() && pq.peek() <= startTime){
-                pq.poll();
-            }
-            
-            pq.offer(endTime+10);
-        }
         
+        for (String[] s : book_time){
+            int startTime = convertToTime(s[0]);  
+            int endTime = convertToTime(s[1]) + 10;
+            
+            if (pq.isEmpty()){
+                pq.offer(endTime);
+            } else {
+                // 다음 시작 시간이 pq에 있는 끝나는 시각 + 10분 보다 작거나 같으면 불가
+                if (startTime < pq.peek()){
+                    pq.offer(endTime);
+                } else {
+                    pq.poll();
+                    pq.offer(endTime);
+                }
+            }
+        }
         return pq.size();
     }
 }
