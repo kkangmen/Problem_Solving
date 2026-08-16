040ee673c518a2f0b57d56d80402fa8ecba2c741
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-16 10:37:57 +0900

    [level 2] Title: 스킬트리, Time: 0.11 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -4,16 +4,29 @@ class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
+        List<String> cleanSkill = new ArrayList<>();
+        
         for (String s : skill_trees){
-            s = s.replaceAll("[^" + skill + "]", "");
-            
-            for (int i = 0; i <= skill.length(); i++){
-                String subStr = skill.substring(0, i);
-                
-                if (s.equals(subStr)){
-                    answer++;
-                    break;
+            String word = s;
+            for (int i = 0; i < s.length(); i++){
+                boolean hasChar = false;
+                for (int j = 0; j < skill.length(); j++){
+                    if (s.charAt(i) == skill.charAt(j)){
+                        hasChar = true;
+                    }
                 }
+                if (!hasChar){
+                    word = word.replace(String.valueOf(s.charAt(i)), "");
+                }
+            }
+            cleanSkill.add(word);
+        }
+        
+        for (String s : cleanSkill){
+            // System.out.println(s);
+            if (skill.startsWith(s)){
+                // System.out.println("증가");
+                answer++;
             }
         }
         return answer;
