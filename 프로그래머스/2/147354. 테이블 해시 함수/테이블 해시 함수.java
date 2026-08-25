369f701fd3edfd8f7279e233116ff2da9857ac76
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-25 14:40:06 +0900

    [level 2] Title: 테이블 해시 함수, Time: 7.61 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -5,10 +5,12 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(data, (o1, o2) -> {
-            if (o1[col-1] == o2[col-1]){
+           int num1 = o1[col-1];
+            int num2 = o2[col-1];
+            if (num1 == num2){
                 return o2[0] - o1[0];
             }
-           return o1[col-1] - o2[col-1]; 
+            return num1 - num2;
         });
         
         for (int i = row_begin-1; i < row_end; i++){
@@ -16,10 +18,8 @@ class Solution {
             for (int j = 0; j < data[i].length; j++){
                 sum += data[i][j] % (i+1);
             }
-            // System.out.println(sum);
-            answer = answer ^ sum;
+            answer = answer^sum;
         }
-        
         return answer;
     }
 }
