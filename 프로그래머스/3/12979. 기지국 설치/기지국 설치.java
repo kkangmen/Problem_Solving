13b1af2d259987e2dbeb228a862dff3b6a6cc49b
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-26 10:37:32 +0900

    [level 3] Title: 기지국 설치, Time: 0.33 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -3,26 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int coverage = 2*w + 1;
-        
-        int pivot = 1;
-        
+    
+        int curSpot = 1;
+        int width = w*2 + 1;
         for (int station : stations){
-            int start = Math.max(0, station-w);
-            int end = Math.min(n, station+w);
+            int gap = (station-w) - curSpot;
             
-            int gap = start - pivot;
-            if (gap > 0){
-                answer += (gap + coverage -1)/ coverage; 
-            }
-            
-            pivot = end+1;
+            answer += (gap + (width-1)) / width;
+            curSpot = station + w + 1;
         }
+        // System.out.println(answer);
         
-        // 끝까지
-        if (pivot <= n){
-            int gap = n - pivot + 1;
-            answer += (gap + coverage -1)/coverage;
+        if (curSpot <= n){
+            // System.out.println("마지막");
+            int gap = n - curSpot + 1;
+            
+            answer += (gap + (width-1))/width;
         }
         return answer;
     }
