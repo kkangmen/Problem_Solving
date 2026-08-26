de0f02de65fa10b7c02f071943c882177d0996b4
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-26 10:52:30 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 9.68 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,38 +1,39 @@
 import java.util.*;
 
 class Solution {
-    
     int[] answer = new int[2];
     
-    public boolean isZipable(int x, int y, int length, int[][] arr){
-        int pivot = arr[x][y];
-        
-        for (int i = x; i < x+length; i++){
-            for (int j = y; j < y+length; j++){
-                if (pivot != arr[i][j]){
-                    return false;
+    public boolean allSame(int row, int col, int len, int[][] arr){
+        boolean flag = true;
+        for (int i = row; i < row+len; i++){
+            for (int j = col; j < col+len; j++){
+                if (arr[row][col] != arr[i][j]){
+                    flag = false;
                 }
             }
         }
-        return true;
+        return flag;
     }
     
-    public void dAC(int x, int y, int length, int[][] arr){
-        
-        if (isZipable(x, y, length, arr)){
-            answer[arr[x][y]]++;
-            return;
+    public void dAQ(int row, int col, int len, int[][] arr){
+        if (allSame(row, col, len, arr)){
+            if (arr[row][col] == 0){
+                answer[0]++;
+            } else {
+                answer[1]++;
+            }
+            return;   
         }
         
-        dAC(x, y, length/2, arr);
-        dAC(x+length/2, y, length/2, arr);
-        dAC(x, y+length/2, length/2, arr);
-        dAC(x+length/2, y+length/2, length/2, arr);
+        dAQ(row, col, len/2, arr);
+        dAQ(row, col+len/2, len/2, arr);
+        dAQ(row+len/2, col, len/2, arr);
+        dAQ(row+len/2, col+len/2, len/2, arr);
     }
     
     public int[] solution(int[][] arr) {
         
-        dAC(0, 0, arr.length, arr);
+        dAQ(0, 0, arr.length, arr);
         return answer;
     }
 }
