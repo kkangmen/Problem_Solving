3aca2e2bae3c850359771d0d9f2fc3f9d564875e
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-08-27 10:23:35 +0900

    [level 3] Title: 최고의 집합, Time: 0.20 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -4,19 +4,20 @@ class Solution {
     public int[] solution(int n, int s) {
         int[] answer = new int[n];
         
-        int quote = s/n;
-        int remain = s%n;
-        
         if (n > s){
             answer = new int[1];
             answer[0] = -1;
             return answer;
         }
+        
         Arrays.fill(answer, s/n);
         
-        for (int i = n-1; i > n-1-remain; i--){
-            answer[i] += 1;
+        int remainder = s%n;
+        
+        for (int i = n-1; i > n-1-remainder; i--){
+            answer[i]++;
         }
+        
         return answer;
     }
 }
