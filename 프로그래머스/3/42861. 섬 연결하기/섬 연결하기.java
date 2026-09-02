198f6882c37f9b8d80d281f1ad7ae806aa01ac0a
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-02 17:15:41 +0900

    [level 3] Title: 섬 연결하기, Time: 0.83 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,68 +2,40 @@ import java.util.*;
 
 class Solution {
     
-    class Node {
-        int start;
-        int end;
-        int cost;
-        Node (int start, int end, int cost){
-            this.start = start;
-            this.end = end;
-            this.cost = cost;
-        }
-    }
-    
     int[] parent;
-    PriorityQueue<Node> pq = new PriorityQueue<>((Node p1, Node p2) -> {
-        return Integer.compare(p1.cost, p2.cost);
-    });
     
-    // union 합치기
-    public void union(int mother, int baby){
-        mother = findParent(mother);
-        baby = findParent(baby);
+    public int findParent(int node){
+        if (parent[node] == node){
+            return node;
+        }    
+        return findParent(parent[node]);
+    }
+    
+    public void union(int a, int b){
+        int parent1 = findParent(a);
+        int parent2 = findParent(b);
         
-        if (mother < baby){
-            parent[baby] = mother;
-        } else {
-            parent[mother] = baby;
-        }
-    }
-    
-    // 부모 찾기
-    public int findParent(int nodeNum){
-        if (parent[nodeNum] == nodeNum){
-            return nodeNum;
-        }
-        return findParent(parent[nodeNum]);
-    }
-    
-    public int getMinimumCost(int answer){
-        while(!pq.isEmpty()){
-            Node node = pq.poll();
-            
-            // 부모가 다르면 간선 추가.
-            if (findParent(node.start) != findParent(node.end)){
-                answer += node.cost;
-                union(node.start, node.end);
-            }
-        }
-        return answer;
+        parent[parent2] = parent1;
     }
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        parent = new int[n];
         
+        parent = new int[n];
         for (int i = 0; i < n; i++){
             parent[i] = i;
         }
         
-        for (int[] cost: costs){
-            pq.add(new Node(cost[0], cost[1], cost[2]));
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+            
+        for (int[] cost : costs){
+            // 부모가 다르면 간선 추가 후 통합
+            if (findParent(cost[0]) != findParent(cost[1])){
+                answer += cost[2];
+                union(cost[0], cost[1]);
+            }
         }
         
-        answer = getMinimumCost(answer);
         return answer;
     }
 }
