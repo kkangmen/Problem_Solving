48501390c6252cfe330571e13f01829448d7cdee
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-17 10:56:01 +0900

    [level 2] Title: [3차] 방금그곡, Time: 5.03 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -3,42 +3,38 @@ import java.util.*;
 class Solution {
     
     public String convert(String s){
-        return s.replace("C#", "c").replace("D#", "d")
-            .replace("F#", "f").replace("G#", "g")
+        return s.replace("C#", "c")
+            .replace("D#", "d")
+            .replace("F#", "f")
+            .replace("G#", "g")
             .replace("A#", "a");
     }
     
-    public int toMinute(String time){
-        int hour = Integer.parseInt(time.split(":")[0]);    
-        int min = Integer.parseInt(time.split(":")[1]);
+    public int toTime(String s){
+        int hour = Integer.parseInt(s.split(":")[0]);
+        int min = Integer.parseInt(s.split(":")[1]);
         
         return hour*60 + min;
     }
     
     public String solution(String m, String[] musicinfos) {
-        String melody = convert(m);
         String answer = "(None)";
         int maxDuration = 0;
         
+        m = convert(m);
+        
         for (String musicinfo : musicinfos){
             String[] arr = musicinfo.split(",");
-            String startTime = arr[0];
-            String endTime = arr[1];
+            int duration = toTime(arr[1]) - toTime(arr[0]);
             String title = arr[2];
-            String info = convert(arr[3]);
+            String music = convert(arr[3]);
             
-            int duration = toMinute(endTime) - toMinute(startTime);
-        
-            // 재생 시간만큼 악보 생성
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < duration; i++){
-                sb.append(info.charAt(i % info.length()));
-            }
+            music = music.repeat(duration/music.length()) + 
+                music.substring(0, duration%music.length());
             
-            // System.out.println(sb.toString());
-            if (sb.toString().contains(melody) && duration > maxDuration){
+            if (music.contains(m) && duration > maxDuration){
+                answer = title;
                 maxDuration = duration;
-                answer = title;       
             }
         }
         return answer;
