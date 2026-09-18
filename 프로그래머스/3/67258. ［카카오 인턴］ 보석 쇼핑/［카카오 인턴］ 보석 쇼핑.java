67991ed566bc1142b270dc037867934ee3b4ccba
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-18 10:25:24 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 25.43 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,51 +1,40 @@
 import java.util.*;
 
 class Solution {
+    
+    Set<String> set = new HashSet<>();
+    Map<String, Integer> map = new HashMap<>();
+    
     public int[] solution(String[] gems) {
-        int[] answer = new int[]{0, gems.length-1};
+        int[] answer = new int[]{1, gems.length};
         
-        Set<String> set = new HashSet<>();
-        
-        for(String gem : gems){
+        for (String gem : gems){
             set.add(gem);
         }
+        int kind = set.size();
         
-        Map<String, Integer> map = new HashMap<>();
         int start = 0;
-        int end = 0;
-        map.put(gems[0], 1);
-        
-        while (true){
+        for (int end = 0; end < gems.length; end++){
+            map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
             
-            // 전체 가짓수 부족할 경우
-            if (map.size() < set.size()){
-                // System.out.println("부족");
-                end++;
-                if (end == gems.length){
-                    break;
-                }
-                map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);    
-            } else { // 전체 가짓수 충분
-                // System.out.println("충분");
-                if (end-start < answer[1]-answer[0]){
-                    answer[0] = start;
-                    answer[1] = end;
-                    // System.out.println(answer[0] + " " + answer[1]);
+            while (map.size() == kind){
+                // System.out.println("start: " + start + " end: " + end);
+                // 값 최신화
+                int gap = answer[1] - answer[0];
+                if (gap > end - start){
+                    answer[0] = start+1;
+                    answer[1] = end+1;
                 }
                 
-                map.put(gems[start], map.get(gems[start]) -1);
+                // 맨 앞 원소 제거
+                map.put(gems[start], map.get(gems[start])-1);
+                
                 if (map.get(gems[start]) == 0){
                     map.remove(gems[start]);
                 }
                 start++;
             }
-            
-            // for (String s : map.keySet()){
-            //     // System.out.println("key: " + s + " value: " + map.get(s));
-            // }
         }
-        answer[0]++;
-        answer[1]++;
         return answer;
     }
 }
