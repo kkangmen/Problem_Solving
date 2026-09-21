0f5eed32eed0e36be1f84a9dbbf03b7f89e7de05
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-21 10:13:36 +0900

    [level 3] Title: 입국심사, Time: 22.72 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,29 +2,38 @@ import java.util.*;
 
 class Solution {
     
-    public long countN(long mid, int[] times){
+    public boolean possible(int n, int[] times, long mid){
         long count = 0;
+        
         for (int time : times){
-            count += mid / time;
+            count += mid / (long)time;
         }
-        return count;
+        
+        if ((long)n <= count){
+            return true;
+        } else {
+            return false;
+        }
     }
     
     public long solution(int n, int[] times) {
         long answer = 0;
         
+        
         long left = 1;
-        Arrays.sort(times);
-        long right = n * (long)times[times.length-1];
+        long right = 0;
+        for (int time : times){
+            right = Math.max(right, time);
+        }
+        right = right*n;
         
         while (left <= right){
             long mid = (left+right)/2;
             
-            long count = countN(mid, times);
-            // System.out.println("mid: " + mid + " count: " + count);
-            if (count >= n){
-                right = mid-1;
+            // 가능한경우
+            if (possible(n, times, mid)){
                 answer = mid;
+                right = mid-1;
             } else {
                 left = mid+1;
             }
