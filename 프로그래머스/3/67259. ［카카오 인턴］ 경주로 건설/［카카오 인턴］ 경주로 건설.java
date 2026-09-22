fa151c709950a7101ac026061b9b2d736ba56794
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-22 10:40:46 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 2.10 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -2,39 +2,42 @@ import java.util.*;
 
 class Solution {
     
-    int[][][] map;
+    int[][][] info;
+    int[][][] cost;
     int[] dx = {0, 1, 0, -1};
     int[] dy = {1, 0, -1, 0};
     Queue<int[]> q = new LinkedList<>();
-    int[][][] costMap;
     
     public void bfs(int row, int col, int[][] board){
         q.offer(new int[]{0, 0, 0});
         q.offer(new int[]{0, 0, 1});
-        costMap[0][0][0] = 0;
-        costMap[0][0][1] = 0;
+        cost[0][0][0] = 0;
+        cost[0][0][1] = 0;
         
         while (!q.isEmpty()){
             int[] curNode = q.poll();
+            // System.out.println(curNode[0] + " " + curNode[1]);
+            
+            int direct = curNode[2];
             for (int i = 0; i < 4; i++){
                 int nx = curNode[0] + dx[i];
                 int ny = curNode[1] + dy[i];
-                if (0 <= nx && nx < row && 0 <= ny && ny < col){
-                    if (board[nx][ny] == 0){
-                        // 이전과 방향이 같을 때
-                        int extraCharge = 0;
-                        if (curNode[2] == i){
-                            extraCharge = 100;
-                        } else {
-                            extraCharge = 600;
-                        }
-                        
-                        // 더 적은 값으로 갈 수 있다면, 갱신하고 추가
-                        int newCost = costMap[curNode[0]][curNode[1]][curNode[2]] + extraCharge;
-                        if (newCost <= costMap[nx][ny][i]){
-                            costMap[nx][ny][i] = newCost;
-                            q.offer(new int[]{nx, ny, i});
-                        }
+                
+                if (0 <= nx && nx < row && 0 <= ny && ny < col
+                   && board[nx][ny] == 0){
+                    // 방향이 같다면
+                    int charge = 0;
+                    if (direct == i){
+                        charge += 100;
+                    } else {
+                        charge += 600;
+                    }
+                    
+                    // 새로운 금액이 더 작다면
+                    if (cost[nx][ny][i] > cost[curNode[0]][curNode[1]][curNode[2]] + charge){
+                        cost[nx][ny][i] = cost[curNode[0]][curNode[1]][curNode[2]] + charge;
+                        // System.out.println(cost[nx][ny][i]);
+                        q.offer(new int[]{nx, ny, i});
                     }
                 }
             }
@@ -42,30 +45,34 @@ class Solution {
     }
     
     public int solution(int[][] board) {
-        int answer = Integer.MAX_VALUE;
         
         int row = board.length;
         int col = board[0].length;
-        map = new int[row][col][4];
-        costMap = new int[row][col][4];
-        
-        // costMap 최대값 채우기
+        info = new int[row][col][4];
+        cost = new int[row][col][4];
         for (int i = 0; i < row; i++){
             for (int j = 0; j < col; j++){
-                Arrays.fill(costMap[i][j], Integer.MAX_VALUE);                
+                Arrays.fill(cost[i][j], Integer.MAX_VALUE);
             }
         }
         
         bfs(row, col, board);
         
+        // print
         // for (int i = 0; i < row; i++){
         //     for (int j = 0; j < col; j++){
-        //         System.out.print(costMap[i][j] + " ");
+        //         int min = cost[i][j][0];
+        //         for (int k = 0; k < 4; k++){
+        //             min = Math.min(min, cost[i][j][k]);
+        //         }
+        //         System.out.print(min + " ");
         //     }
         //     System.out.println();
         // }
-        for (int i = 0; i < 4; i++){
-            answer = Math.min(answer, costMap[row-1][col-1][i]);
+        
+        int answer = cost[row-1][col-1][0];
+        for (int i = 1; i < 4; i++){
+            answer = Math.min(answer, cost[row-1][col-1][i]);
         }
         return answer;
     }
