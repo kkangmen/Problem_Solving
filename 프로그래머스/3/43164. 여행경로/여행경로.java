330741fedc735cab3c7524fa4506e5bf1289f97e
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-26 10:40:53 +0900

    [level 3] Title: 여행경로, Time: 0.65 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,45 +1,55 @@
 import java.util.*;
 
 class Solution {
-    Map<String, List<String>> map = new HashMap<>();
-    Map<String, boolean[]> isVisited = new HashMap<>();
-    boolean flag = false;
-    List<String> answer = new ArrayList<>();
     
-    public void dfs(String point, String[][] tickets, List<String> cur){
-        cur.add(point);                        // 수정 1
+    boolean[] isVisited;
+    boolean flag = false;
+    List<String> answer;
+    
+    public void dfs(String dest, List<String> cur, String[][] tickets){
         
-        if (cur.size() == tickets.length + 1){
+        cur.add(dest);
+        
+        // 전체 다 갈 수 있는 경로일 경우
+        if (cur.size() == tickets.length+1){
             answer = new ArrayList<>(cur);
             flag = true;
             return;
         }
         
-        List<String> nexts = map.getOrDefault(point, Collections.emptyList());  // 수정 3
-        boolean[] visited = isVisited.getOrDefault(point, new boolean[0]);
-        
-        for (int i = 0; i < nexts.size(); i++){
-            if (!visited[i]){
-                visited[i] = true;
-                dfs(nexts.get(i), tickets, cur);
-                if (flag) return;
-                visited[i] = false;
+        for (int i = 0; i < tickets.length; i++){
+            String[] ticket = tickets[i];
+            
+            if (ticket[0].equals(dest) && !isVisited[i]){
+                isVisited[i] = true;
+                dfs(ticket[1], cur, tickets);
+                if (flag){
+                    return;
+                }
+                isVisited[i] = false;
             }
         }
         
-        cur.remove(cur.size() - 1);            // 수정 2
+        cur.remove(cur.size()-1);
     }
     
     public String[] solution(String[][] tickets) {
-        for (String[] ticket : tickets){
-            map.computeIfAbsent(ticket[0], k -> new ArrayList<>()).add(ticket[1]);
-        }
-        for (String s : map.keySet()){
-            Collections.sort(map.get(s));                          // 정렬 먼저
-            isVisited.put(s, new boolean[map.get(s).size()]);
+        List<String> cur = new ArrayList<>();
+        
+        Arrays.sort(tickets, (o1, o2) -> o1[1].compareTo(o2[1]));
+        isVisited = new boolean[tickets.length];
+        
+        for (int i = 0; i < tickets.length; i++){
+            String[] ticket = tickets[i];
+            if (ticket[0].equals("ICN") && !flag){
+                cur.add("ICN");
+                isVisited[i] = true;
+                dfs(ticket[1], cur, tickets);
+                cur.remove(cur.size()-1);
+                isVisited[i] = false;
+            }    
         }
         
-        dfs("ICN", tickets, new ArrayList<>());
-        return answer.toArray(new String[0]);  // 수정 5
+        return answer.toArray(new String[answer.size()]);
     }
 }
